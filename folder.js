b711395c9bcbb28fc4e495e080c7297421e1df4d
woosukkim <ipadorusa@gmail.com>
2020-11-09 18:41:46 +0900

    json update

--- folder.js
+++ folder.js
@@ -13,13 +13,21 @@ function readFromFile(file) {
 		});
 	});
 }
-const url = './public/year/41465/20202019.json';
+const url = './public/year/41463/20202019.json';
 
-let list = readFromFile(url).then(res => console.log(res));
-/*
-let aptList = [];
-for (const item of d) {
-	aptList.push(item['아파트']);
+readFromFile(url).then(res => getList(res));
+
+function sol(arr) {
+	return arr.reduce((sum, cur) => sum + parseInt(cur), 0) / arr.length;
 }
-let uniAptList = [...new Set(aptList)];
-*/
+
+async function getList(res) {
+	let data = await res;
+	let filterData = filter(data.flat(), { 년: 2020, 아파트: '영통로효성해링턴플레이스1차', 전용면적: 59.963 });
+	let filterData2 = filter(filterData, o => o['월'] > 1);
+	let filteList = [];
+	for (let item of filterData) {
+		filteList.push(item['거래금액'].replace(/\,/g, ''));
+	}
+	console.log(sol(filteList));
+}
