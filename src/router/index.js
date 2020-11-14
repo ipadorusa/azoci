47474c35f8ec57f7ba6f73f7fed97432d0f0ce88
woosukkim <ipadorusa@gmail.com>
2020-11-15 00:40:00 +0900

    data 부분 처리

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ const routes = [
 		path: '/',
 		name: 'Home',
 		component: Home,
+		meta: {
+			title: '수지구,기흥구 아파트 월별 실거래가 검색',
+		},
 	},
 	{
 		path: '/DashBoard',
@@ -17,6 +20,9 @@ const routes = [
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "DashBoard" */ '../views/DashBoard.vue'),
+		meta: {
+			title: '수지구,기흥구 아파트 2019년 2020년 실거래가 검색',
+		},
 	},
 ];
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+module.exports = {
+	chainWebpack: config => {
+		config.plugin('html').tap(args => {
+			args[0].title = '수지구,기흥구 아파트 월별 실거래가 검색';
+			return args;
+		});
+	},
+};
+
 /*
 module.exports = {
 	devServer: {
